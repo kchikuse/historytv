2abc0cf03e7730fa5fec266e826caea11f8c133b
Knowledge Chikuse <Knowledge.Chikuse@standardbank.co.za>
2020-09-27 12:00:51 +0200

    shiny updates

--- preload.js
+++ preload.js
@@ -1,55 +1,36 @@
 addEventListener("DOMContentLoaded", () => {
-    const fs = require("fs");
     
-    let timeline,
-        isTurnedOn = true,
-        playlist = getAllYears(),
-        state = loadState();
-
+    const fs = require("fs");
+    const http = require("http");
     const player = videojs("video");
-    player.currentTime(state.time);
+
+    let timeline,
+        playlist = getAllYears(),
+        state = new Proxy({ index: 0, volume: 0, time: 0}, {
+            set: function (target, key, value) {
+                target[key] = value;
+                updatePlayer();
+                return true;
+            }
+    });
+
     player.autoplay(true);
     player.preload(true);
-    player.volume(0);
     player.playlist(playlist);
     player.playlist.repeat(true);
-    player.playlist.currentItem(state.index);
     player.playlist.autoadvance(0);
 
-    player.on("play", () => {
-        let src = player.currentSrc();
-        let video = playlist.find(p => p.sources[0].src == src);
-        showYear(video.year);
-        //console.log(video);
+    player.on("volumechange", () => {
+        $("button")[ player.volume() == 0 ? "show" : "hide" ]();
     });
 
-    player.on("useractive", () => {
-        $("button").show();
-    });
+    $(window).on("beforeunload", saveState);
 
-    player.on("userinactive", () => {
-        if(!player.paused()) {
-            $("button").hide();
-        }
-    });
-
-    $("button").on("click", () => {
-        if (isTurnedOn) {
-            turnOff();
-        }
-        
-        if (!isTurnedOn) {
-            turnOn();
-        }
-        
-        isTurnedOn = !isTurnedOn;
-    });
-
-    setInterval(saveState, 2000);
-
-    showYear(playlist[state.index].year);
+    loadState();
 
     buildTimeline();
+
+    controlServer();
 
     function getAllYears() {
         const DIR = "./assets/videos/";
@@ -74,23 +55,45 @@ addEventListener("DOMContentLoaded", () => {
         return list;
     }
 
-    function getYear(year) {
-        return getAllYears().filter(y => y.year == year);
-    }
-
     function showYear(year) {
         $("calendar").html(year);
     }
 
     function loadState() {
-        let store = localStorage.getItem("state");
-        return store ? JSON.parse(store) : { index: 0, time: 0 };
+        try {
+            let src = player.currentSrc();
+            let video = playlist.find(p => p.sources[0].src == src);
+            let store = JSON.parse(localStorage.getItem("state") || {});
+
+            state.on = store.on == false ? false : true;
+            state.year = store.year || video.year;
+            state.volume = store.volume || 0;
+            state.index = store.index || 0;
+            state.time = store.time || 0; 
+        }
+        catch(e) {}
     }
 
     function saveState() {
-        state.index = player.playlist.currentItem();
-        state.time = player.currentTime();
-        localStorage.setItem("state", JSON.stringify(state));
+        let store = Object.assign(state, {
+            time: player.currentTime()
+        });
+
+        localStorage.setItem("state", JSON.stringify(store));
+    }
+
+    function updatePlayer() {
+        if(player.playlist.currentItem() != state.index) {
+            player.playlist.currentItem(state.index);
+        }
+
+        if(player.volume() != state.volume) {
+            player.volume(state.volume);
+        }
+
+        player.currentTime(state.time);
+        
+        showYear(state.year);
     }
 
     function buildTimeline() {
@@ -114,31 +117,84 @@ addEventListener("DOMContentLoaded", () => {
 
     function turnOff() {
         player.pause();
-
-        $("calendar").attr("cloak", true);
         
-        $(".video-js").fadeOut("fast", function(){
-            $(this).attr("cloak", true);
-            $("button").text("ON");
+        $(".video-js").fadeOut("fast", () => {
+            $(".video-js, calendar").attr("cloak", true);
             timeline.restart();
         });
     }
 
     function turnOn() {
         timeline.reverse();
-        
-        $(".video-js").fadeIn("fast", function() {
-            $(this).removeAttr("cloak");
-            $("calendar").removeAttr("cloak");
-            $("button").text("OFF");
-            
-            if(player.paused()) {
-                player.play();
-            }
+
+        $(".video-js").fadeIn("fast", () => {
+            $(".video-js, calendar").removeAttr("cloak");
+            player.play();
         });
     }
 
-    function l(s) {
+    function process(data) {
+        try {
+            let e = JSON.parse(data);
+            
+            if(e.volume <= 1 && e.volume >= 0) {
+                state.volume = e.volume;
+            }
+        
+            if(e.index) {
+                state.year = playlist[e.index].year;
+                state.index = e.index;
+            }
+
+            if(e.year) {
+                state.index = playlist.findIndex(i =>  i.year == state.year);
+                state.year = e.year;
+                state.time = 0;
+            }
+
+            if(e.on == true && state.on == false) {
+                state.on = true;
+                turnOn();
+            }
+
+            if(e.on == false && state.on == true) {
+                state.on = false;
+                turnOff();
+            }
+        }
+        catch(e) {}
+    }
+
+    function controlServer() {
+        http.createServer((r, s) => {     
+            let body = "";
+            
+            r.on("end", () => {
+                
+                if(r.method == "POST") {
+                    process(body);
+                }
+
+                let output = Object.assign({}, state, {
+                    playlist: playlist.length,
+                    duration: player.duration(),
+                    title: playlist[state.index].sources[0].src
+                });
+
+                s.setHeader("access-control-allow-origin", "*");
+                s.setHeader("content-type", "application/json");
+                s.write(JSON.stringify(output));
+                s.end();
+            });
+
+            r.on("data", chunk => {
+                body += chunk;
+            });
+
+        }).listen(8000); 
+    }
+
+    function _(s) {
         console.log(s);
     }
 });
